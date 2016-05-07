bdd382d79654722cc2d315499cd5eccf8756736b
Aditya Atluri <adityaavinash1@gmail.com>
2016-05-07 16:04:36 -0500

    Added simple vulkan example

---
+++ example-01/vkDevices/vkDevices/Source.cpp
@@ -0,0 +1,30 @@
+#include"vulkan\vulkan.h"
+#include<iostream>
+#include<vector>
+
+int main()
+{
+	VkResult result;
+	VkApplicationInfo appInfo = {};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "VulkanTriangle";
+	appInfo.pEngineName = "GPU0";
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	std::vector<const char*> enabledExtensions = { VK_KHR_SURFACE_EXTENSION_NAME };
+
+	VkInstanceCreateInfo instanceCreateInfo = {};
+	instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceCreateInfo.pNext = NULL;
+	instanceCreateInfo.pApplicationInfo = &appInfo;
+	instanceCreateInfo.enabledExtensionCount = (uint32_t)enabledExtensions.size();
+	instanceCreateInfo.ppEnabledExtensionNames = enabledExtensions.data();
+
+	VkInstance pInstance;
+	result = vkCreateInstance(&instanceCreateInfo, NULL, &pInstance);
+
+	uint32_t gpuCount = 0;
+	vkEnumeratePhysicalDevices(pInstance, &gpuCount, nullptr);
+	std::cout << "Number of Vulkan capable GPUs: " << gpuCount << std::endl;
+	std::cout << "Exiting.. " << std::endl;
+}
